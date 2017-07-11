858792ebc6ce2a925176b7e8300fbc8695bac1d0
Matthew Hummer <biggreyson@gmail.com>
2017-07-11 11:48:31 -0600

    fix semicolons

--- app/public/components/newPost/newpost.component.js
+++ app/public/components/newPost/newpost.component.js
@@ -20,62 +20,50 @@ function postcontroller($http, postsService, commentsService){
     }else{
       vm.posts[postNumber].commentsVisible = true;
     }
-  }
+  };
   vm.checkSort = function(){
     switch (vm.sort){
       case '-vote_count':
-        return 'Votes'
-        break;
+        return 'Votes';
       case 'Date':
         return 'date';
-        break;
       case 'title':
       return 'Title';
-        break;
     }
-  }
+  };
   vm.toggle = function(){
     if (vm.makingNew){
       vm.makingNew = false;
     }else{
       vm.makingNew = true;
     }
-  }
+  };
   vm.toggleCommentEditor= function(event, post, comment){
-    let postNumber = vm.posts.indexOf(post)
+    let postNumber = vm.posts.indexOf(post);
     let commentNumber = vm.posts[postNumber].comments.indexOf(comment);
     if (vm.posts[postNumber].comments[commentNumber].editing){
       vm.posts[postNumber].comments[commentNumber].editing=false;
     }else{
       vm.posts[postNumber].comments[commentNumber].editing=true;
     }
-  }
+  };
   vm.addComment = commentsService.addComment;
   vm.editComment=function(post, comment, edit){
-    let postNumber = vm.posts.indexOf(post)
+    let postNumber = vm.posts.indexOf(post);
     let commentNumber = vm.posts[postNumber].comments.indexOf(comment);
     vm.toggleCommentEditor(event, post, comment);
     $http.patch(`/api/posts/${post.id}/comments/${comment.id}`, {content: edit})
     .then(function(response){
       vm.posts[postNumber].comments[commentNumber].content = response.data.content;
     });
-  }
+  };
   vm.deleteComment = commentsService.deleteComment;
-  // vm.deleteComment=function(event,post, comment){
-  //   let postNumber = vm.posts.indexOf(post)
-  //   let commentNumber = vm.posts[postNumber].comments.indexOf(comment);
-  //   $http.delete(`/api/posts/${post.id}/comments/${comment.id}`)
-  //   .then(function(response){
-  //     console.log(response);
-  //     vm.posts[postNumber].comments.splice(commentNumber, 1);
-  //   })
-  // }
   vm.$onInit = function(){
     postsService.getAllPosts().then(function(post){
       vm.posts = post;
-    })
+    });
     vm.sort = '-vote_count';
     vm.makingNew = false;
-  }
+  };
 }
 }());
--- app/public/services/comments.js
+++ app/public/services/comments.js
@@ -6,17 +6,17 @@
 
     };
     this.addComment = function(post, comment){
-      let postNumber = this.posts.indexOf(post)
+      let postNumber = this.posts.indexOf(post);
       return $http.post(`/api/posts/${post.id}/comments`, {content: comment}).then((response)=>{
         return this.posts[postNumber].comments.push(response.data);
-      })
-    }
+      });
+    };
     this.deleteComment = function(post, comment){
       let postNumber = this.posts.indexOf(post);
       let commentNumber = this.posts[postNumber].comments.indexOf(comment);
       return $http.delete(`/api/posts/${post.id}/comments/${comment.id}`).then((response)=>{
         return this.posts[postNumber].comments.splice(commentNumber, 1);
       });
-    }
+    };
   }
 }());
